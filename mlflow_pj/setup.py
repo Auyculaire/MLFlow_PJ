ee9293fad95b8c6cc472a554e022e4a3f7f4c2fb
Auryculaire <alexandre.poloubinski@efrei.net>
2023-01-26 15:17:26 +0100

    first commit

---
+++ mlflow_pj/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MlFlow project M2 - Data Engineering 2022-2023',
+    author='Alexandre Poloubinski',
+    license='',
+)
